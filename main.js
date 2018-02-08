b7d58dc08797b80c5966f22d0781521932a1762e
Rea Newell <patrick.r.newell@gmail.com>
2018-02-08 11:02:26 +0900

    added popup image functionality

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // VARIABLES //
 var phoebe = document.getElementById('phoebe');
 var mario = document.getElementById('mario');
-
+var popupImage = document.getElementById('popup-image');
 
 // GALLERY MECHANICS //
 if (window.innerWidth > 720) {
     function showPhoebeGallery() {
         document.getElementById('phoebe-gallery').style.right = '0%';
-        document.getElementById('phoebe').id = 'phoebe-stationary';
+        phoebe.id = 'phoebe-stationary';
     };
     function closePhoebeGallery() {
         document.getElementById('phoebe-gallery').style.right = '-100%';
@@ -15,14 +15,14 @@ if (window.innerWidth > 720) {
     };
     function showMarioGallery() {
         document.getElementById('mario-gallery').style.left = '0%';
-        document.getElementById('phoebe').style.position = 'absolute';
-        document.getElementById('phoebe').style.right = '100%';
-        document.getElementById('mario').id = 'mario-stationary';
+        phoebe.style.position = 'absolute';
+        phoebe.style.right = '100%';
+        mario.id = 'mario-stationary';
     };
     function closeMarioGallery() {
         document.getElementById('mario-gallery').style.left = '-100%';
-        document.getElementById('phoebe').style.position = 'relative';
-        document.getElementById('phoebe').style.right = 'auto';
+        phoebe.style.position = 'relative';
+        phoebe.style.right = 'auto';
         document.getElementById('mario-stationary').id = 'mario';
     };
 } else {
@@ -39,3 +39,20 @@ if (window.innerWidth > 720) {
         document.getElementById('mario-gallery').style.top = '100vh';
     };
 }
+
+
+document.onclick = function(e) {
+    if (e.target.tagName == 'IMG') {
+        var img = document.createElement('img');
+        var image = e.target.getAttribute("src");
+        img.setAttribute("src", image);
+
+        popupImage.style.display = "flex";
+        popupImage.appendChild(img);
+        var image = "";
+    };
+};
+popupImage.addEventListener('click', function() {
+    popupImage.style.display = "none";
+    popupImage.innerHTML = "";
+});
